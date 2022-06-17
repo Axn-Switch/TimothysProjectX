574f93784260c2c033f8f44297191f13d81e4f41
utibe inyangetuk <inyangutibz@gmail.com>
2022-06-17 14:51:33 +0100

    project setup

---
+++ src/views/home.vue
@@ -0,0 +1,5 @@
+<template>
+	<router-link to="/login">Login</router-link> |
+	<router-link to="/signup">signup</router-link>
+	<h2>Homepage</h2>
+</template>
---
+++ src/views/login.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<h2>login page</h2>
+	</div>
+</template>
