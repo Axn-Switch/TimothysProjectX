fdb7103ed30f15ecf599019466ab1bfabb7ab9d5
utibe inyangetuk <inyangutibz@gmail.com>
2022-07-04 15:05:47 +0100

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,12 @@
 </template>
 
 <style>
-@import url(" https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;700&display=swap ");
-#app {
-	font-family: poppins;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: poppins;
-	list-style: none;
-}
+	@import url(' https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;700&display=swap ');
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: poppins;
+		list-style: none;
+	}
 </style>
