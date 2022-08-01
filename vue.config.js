8aae78ae30ce25ef3911b36a1da62c7e29d33375
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-01 23:27:12 +0100

    e no gads much

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
 })
 
 module.exports = {
-  devServer: {
-      proxy: {
-          '^/api': {
-              target: 'http://localhost:5000',
-              changeOrigin: true,
-              logLevel: 'debug',
-              pathRewrite: {'^/api': '/'},
-          }
-      }
-  }
+	devServer: {
+		proxy: {
+			'^/api': {
+				target: 'http://localhost:5000',
+			},
+		},
+	},
 }
