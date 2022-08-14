dc7d8eeff88d37fb7eb9a861446ccbaf58543b79
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-14 20:41:18 +0100

    404

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,32 +1,37 @@
 <template>
-	<div class="notfound">
-		<p>Oops! You just hit a Four-Oh-Four.</p>
-	</div>
-	<div class="redirect">
-		<router-link class="btn" :to="{ name: 'home' }"> Take me home </router-link>
+	<div class="container">
+		<h1>oops! you just hit a four-oh-four</h1>
+		<router-link class="btn" :to="{ name: 'home' }">Take me home</router-link>
 	</div>
 </template>
 
-<style scoped>
-	.notfound p {
-		margin-top: 20px;
-		font-size: 10px;
-		letter-spacing: 2px;
-	}
-	.redirect {
-		justify-content: center;
+<style scope>
+	.container {
+		height: 100vh;
 		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		font-family: poppins;
+	}
+
+	.container h1 {
+		letter-spacing: 2px;
+		text-transform: uppercase;
+		font-size: 20px;
 	}
 	.btn {
-		color: #999;
-		border: 1px solid rgba(153, 153, 153, 0.285);
-		padding: 10px;
-		font-size: 10px;
-		font-weight: bold;
+		border: 1px solid #000;
+		color: #000;
 		border-radius: 5px;
-		margin-top: 20px;
+		padding: 10px;
+		letter-spacing: 2px;
+		font-weight: bold;
+		text-transform: uppercase;
+		margin-top: 15px;
 	}
 	.btn:hover {
-		text-decoration: underline;
+		background-color: #000;
+		color: #fff;
 	}
 </style>
