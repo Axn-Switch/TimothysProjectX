7d6ba9f553aa6898d61c12fdd69e3004f3dcb24a
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-17 00:23:47 +0100

    password reset complete

--- src/views/passwordreset.vue
+++ src/views/passwordreset.vue
@@ -1,13 +1,16 @@
 <template>
 	<div class="container">
 		<div class="wrapper">
-			<span class="logo">Enter a new password</span>
+			<span class="logo">password reset</span>
 			<form @submit.prevent.trim="reset">
 				<div class="input-field">
-					<input type="text" placeholder=" enter a new password" v-model="passcheck" required />
+					<input type="text" placeholder=" username" v-model="username" required />
 				</div>
 				<div class="input-field">
-					<input type="password" placeholder="re enter your new password" v-model="passcheck2" required />
+					<input type="password" placeholder=" enter new password" v-model="password" required />
+				</div>
+				<div class="input-field">
+					<input type="password" placeholder="confirm new password" v-model="password2" required />
 				</div>
 				<div class="button">
 					<button type="submit">reset</button>
@@ -21,24 +24,25 @@
 	import axios from 'axios'
 	import { ref } from 'vue'
 	import { useRouter } from 'vue-router'
+	const router = useRouter()
 
 	const username = ref('')
 	const password = ref('')
-	const BaseUrl = ref(`http://localhost:5000/users`)
-	const router = useRouter()
+	const password2 = ref('')
 
-	const login = async () => {
-		await axios
-			.get(`${BaseUrl.value}?username=${username.value}&password=${password.value}`)
-			.then((response) => {
-				console.log(response.data[0].role)
-				if (response.data[0].role === 'admin') {
-					router.push('/account/broadcasts')
-				} else {
-					router.push('/account/userhome')
-				}
+	const reset = async (id) => {
+		let passReset = {
+			password: '',
+		}
+		if (password.value != password2.value) {
+			alert('passwords do not match')
+		} else {
+			axios.get(`${`http://localhost:5000/users`}?username=${username.value}`).then((response) => {
+				id = response.data[0].id
+				passReset.password = password.value
+				axios.patch(`${`http://localhost:5000/users`}/${id}`, passReset)
 			})
-			.catch(() => alert('username or password incorrect'))
+		}
 	}
 </script>
 
@@ -79,11 +83,12 @@
 	}
 	button {
 		margin-top: 10px;
-		padding: 5px 50px;
+		padding: 5px 100px;
 		cursor: pointer;
 		font-size: 15px;
 		font-weight: bold;
 		text-transform: uppercase;
+		letter-spacing: 2px;
 		background: rgba(79, 166, 253, 0.767);
 		border: none;
 		border-radius: 5px;
